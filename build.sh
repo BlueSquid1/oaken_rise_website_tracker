3df378578e3ac552edd7f04fe1ad15c1b92bb3f5
Clinton Page <clinton.page123@gmail.com>
2024-04-07 16:59:30 +0930

    silly relative paths

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf ${SCRIPT_DIR}/venv
 python3 -m venv ${SCRIPT_DIR}/venv
-${SCRIPT_DIR}/venv/bin/pip3 install -r ./requirements.txt
+${SCRIPT_DIR}/venv/bin/pip3 install -r ${SCRIPT_DIR}/requirements.txt
